f983fcc91b94ba2b1269549ff25efc3bad550045
Bippserino <bobomandic2808@gmail.com>
2021-01-02 00:00:08 +0100

    Changed design and fixed bugs

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ class Slider {
     constructor() {
         this.offsetArray = new Array()
         this.currentImage = $(".row-1 img:first-child").attr("src")
-        this.sliderPositon = $(".slider").offset()["left"]
-        this.sliderWidth = parseFloat($(".slider").css("width").slice(0,-2),10)
+        this.sliderPositon = $(".container__slider").offset()["left"]
+        this.sliderWidth = parseFloat($(".container__slider").css("width").slice(0,-2),10)
     }
     
     // Get all pictures and their positions and save it in format [picture name, left edge offset, right edge offset] and get slider position
@@ -30,29 +30,14 @@ class Slider {
             return 0;
         })
 
-        this.sliderPositon = $(".slider").offset()["left"]
-        this.sliderWidth = parseFloat($(".slider").css("width").slice(0,-2),10)
+        this.sliderPositon = $(".container__slider").offset()["left"]
+        this.sliderWidth = parseFloat($(".container__slider").css("width").slice(0,-2),10)
         this.offsetArray = pictures
-    }
-
-    // Set width of a slider row
-    setSliderWidth() {
-        let picturesWidth = 0
-        let pictures = $(".slider img")
-        for (let i = 0; i< pictures.length; i++) {
-          picturesWidth += $(pictures).width()
-        }
-        $(".slider__row").css("width",picturesWidth + "px")
-    }
-
-    // Get picture width
-    getPictureWidth(picture) {
-        return parseInt($(`img[src$='${picture}']`).css("width").slice(0,-2),10)
     }
 
     // Disable buttons and enable them after animation is done
     disableAndEnableButtons() {
-        $(".slider--control").off()
+        $(".button").off()
         setTimeout(() => {
             this.enableButtons()
         },1000)
@@ -77,10 +62,10 @@ class Slider {
     checkIfInsideViewport() {
         let previousOffsetRow1; let previousOffsetRow2; let pictureOutOfViewport; let pictureHTML; let pictureParent;
         this.getOffsetArray()
-        $(".slider__row").css("transition","none")
+        $(".container__slider--row-images").css("transition","none")
 
         for (let i = 0; i < this.offsetArray.length; i++) {
-            if ((this.sliderPositon + this.sliderWidth - 5) <= this.offsetArray[i][1] ) {
+            if ((this.sliderPositon + this.sliderWidth) <= this.offsetArray[i][1] ) {
                 
                 previousOffsetRow1 = parseInt($(".row-1").css("left").slice(0,-2),10)
                 previousOffsetRow2 = parseInt($(".row-2").css("left").slice(0,-2),10)
@@ -90,12 +75,11 @@ class Slider {
 
                 // Reduce the offset of a row by a picture width and a margin
                 if (pictureParent.classList[1] === "row-1") {
-                    $(pictureParent).css("left",`${previousOffsetRow1 - (pictureOutOfViewport[2] - pictureOutOfViewport[1]) - 5}px`)
+                    $(pictureParent).css("left",`${previousOffsetRow1 - (pictureOutOfViewport[2] - pictureOutOfViewport[1]) - 8}px`)
                 }
                 else {
-                    $(pictureParent).css("left",`${previousOffsetRow2 - (pictureOutOfViewport[2] - pictureOutOfViewport[1]) - 5}px`)
+                    $(pictureParent).css("left",`${previousOffsetRow2 - (pictureOutOfViewport[2] - pictureOutOfViewport[1]) - 8}px`)
                 }
-
                 this.removePicture(pictureOutOfViewport[0])
                 this.addPictureToBack(pictureHTML, pictureParent)    
             }
@@ -130,68 +114,80 @@ class Slider {
         }
     }
 
-    // Enables button event listener
-    enableButtons() {
-        $(".slider--control").click((event) => {
-            let previousOffsetRow1 = parseInt($(".row-1").css("left").slice(0,-2),10)
-            let previousOffsetRow2 = parseInt($(".row-2").css("left").slice(0,-2),10)
+    // At first, only enable next button
+    firstClick() {
+        $(".next").click((event) => {
+            $(".next").off()
+            $(".gray").css("background-image",`url("img/arrow-blue-left.png")`)
+            $(".gray").removeClass("gray")
+            this.buttonsHandler(event)
+            setTimeout(() => {
+                this.enableButtons()
+            }, 1000)
+        })
+    }
 
+    buttonsHandler(event) {
+        let previousOffsetRow1 = parseInt($(".row-1").css("left").slice(0,-2),10)
+        let previousOffsetRow2 = parseInt($(".row-2").css("left").slice(0,-2),10)
+        this.getOffsetArray()
+
+        $(".container__slider--row-images").css("transition","1s")
+
+        if (event.target.classList.contains("next")) {
+            let nextPictureOffset = this.findNextPicture()
+            $(".row-1").css("left",`${previousOffsetRow1 + nextPictureOffset}px`)
+            $(".row-2").css("left", `${previousOffsetRow2 + nextPictureOffset}px`)
+        }
+        
+        else if (event.target.classList.contains("previous")) {
+            // Disable animations while in preparation
+            $(".container__slider--row-images").css("transition","none")
+            
+            // Add last pictures of each row to the front
+            this.addPictureToFront($(`.row-1 img:last-child`)[0].outerHTML, $(".row-1"))
+            this.addPictureToFront($(`.row-2 img:last-child`)[0].outerHTML, $(".row-2"))
+            
+            // Prepare it for animation transition
+            $(".row-1").css("left", `${previousOffsetRow1 + $(`.row-1 img:last-child`).width() + 5}px`)
+            $(".row-2").css("left", `${previousOffsetRow2 + $(`.row-2 img:last-child`).width() + 5}px`)
+            
             this.getOffsetArray()
 
-            $(".slider__row").css("transition","1s")
+            // Turn on animation transition and refresh row offsets
+            $(".container__slider--row-images").css("transition","1s")
+            previousOffsetRow1 = parseInt($(".row-1").css("left").slice(0,-2),10)
+            previousOffsetRow2 = parseInt($(".row-2").css("left").slice(0,-2),10)
 
-            if (event.target.classList[1] === "next") {
-                let nextPictureOffset = this.findNextPicture()
-                $(".row-1").css("left",`${previousOffsetRow1 + nextPictureOffset}px`)
-                $(".row-2").css("left", `${previousOffsetRow2 + nextPictureOffset}px`)
-            }
+            // Find picture to go back to and move animation
+            let previousPicture = this.findPreviousPicture()
+            $(".row-1").css("left",`${previousOffsetRow1 - (previousPicture - (this.sliderPositon + this.sliderWidth))}px`)
+            $(".row-2").css("left", `${previousOffsetRow2 - (previousPicture - (this.sliderPositon + this.sliderWidth))}px`)
             
-            else if (event.target.classList[1] === "previous") {
-                // Disable animations while in preparation
-                $(".slider__row").css("transition","none")
-                
-                // Add last pictures of each row to the front
-                this.addPictureToFront($(`.row-1 img:last-child`)[0].outerHTML, $(".row-1"))
-                this.addPictureToFront($(`.row-2 img:last-child`)[0].outerHTML, $(".row-2"))
-                
-                // Prepare it for animation transition
-                $(".row-1").css("left", `${previousOffsetRow1 + $(`.row-1 img:last-child`).width() + 5}px`)
-                $(".row-2").css("left", `${previousOffsetRow2 + $(`.row-2 img:last-child`).width() + 5}px`)
-                
-                this.getOffsetArray()
-
-                // Turn on animation transition and refresh row offsets
-                $(".slider__row").css("transition","1s")
-                previousOffsetRow1 = parseInt($(".row-1").css("left").slice(0,-2),10)
-                previousOffsetRow2 = parseInt($(".row-2").css("left").slice(0,-2),10)
-
-                // Find picture to go back to and move animation
-                let previousPicture = this.findPreviousPicture()
-                $(".row-1").css("left",`${previousOffsetRow1 - (previousPicture - (this.sliderPositon + this.sliderWidth))}px`)
-                $(".row-2").css("left", `${previousOffsetRow2 - (previousPicture - (this.sliderPositon + this.sliderWidth))}px`)
-                
-                // After animation remove them
-                setTimeout(() => {
-                    $(`.row-1 img:last-child`).remove()
-                    $(`.row-2 img:last-child`).remove() 
-                }, 1000)
-                
-            }
-            
-            // After animations are done, remove those outside of the viewport on the right side of the screen
+            // After animation remove them
             setTimeout(() => {
-                this.checkIfInsideViewport()
+                $(`.row-1 img:last-child`).remove()
+                $(`.row-2 img:last-child`).remove() 
             }, 1000)
-
-            // Disable button press and enable after animation is done
-            this.disableAndEnableButtons()
-        })
             
+        }
+        
+        // After animations are done, remove those outside of the viewport on the right side of the screen
+        setTimeout(() => {
+            this.checkIfInsideViewport()
+        }, 1000)
+    }
+
+    // Enables button event listener
+    enableButtons() {
+        $(".button").click((event) => {
+            this.disableAndEnableButtons()
+            this.buttonsHandler(event)
+        })
     }
 }
 var slider
 $(document).ready(() => {
     slider = new Slider()
-    slider.enableButtons()
-    slider.setSliderWidth()
+    slider.firstClick()
 });
